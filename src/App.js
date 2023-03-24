07dd2c4340d54d42c6920d509d70a0df2f7db1f0
JRendl <joe@cojilas.com>
2023-03-24 09:51:21 -0500

    initial animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactAnime from 'react-animejs';
+const {Anime, stagger} = ReactAnime;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Anime
+        initial={[
+          {
+            targets: "#Box",
+            translateX: 50,
+            easing: "linear"
+          }
+        ]}
+      >
+        <div id="Box" style={{ height: 50, width: 50, background: "#d3d" }} />
+      </Anime> */}
+      {/* <Anime
+        initial={[
+          {
+            targets: "#Box",
+            keyframes: [
+              {
+                translateX: 50
+              },
+              {
+                translateY: 50
+              },
+              {
+                translateX: 0
+              },
+              {
+                translateY: 0
+              }
+            ],
+            // easing:'spring',
+            duration: 3500,
+            loop: true
+          }
+        ]}
+      > */}
+      <Anime
+        initial={[
+          { //1st segment
+            targets: ".tl_square",
+            translateX: 250
+          },
+          { //2nd
+            targets: ".tl_circle",
+            translateX: 250
+          },
+          { //3rd
+            targets: ".tl_triangle",
+            translateX: 250
+          }
+        ]}
+      >
+          <div className="tl_square" style={{ height: 50,  width:50, background:'#d3f454'}}  ></div>
+          <div className="tl_circle" style={{ height: 50, width:50, background:'#d3f454', borderRadius: '50%'}}></div>
+          <div className="tl_triangle" style={{height: 50,  width:50, background:'#d3f454', clipPath: 'polygon(50% 0, 0 100%, 100% 100%)'}}></div>
+
+        {/* <div id="Box" style={{ height: 50, width: 50, background: "#d3d" }} /> */}
+        {/* <Box /> */}
+      </Anime>
+
+      
     </div>
   );
 }
