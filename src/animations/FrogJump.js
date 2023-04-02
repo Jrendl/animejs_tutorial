7b0f50c2b8086bc52eae25b1c3cdf8a0b3c7deb3
Joe Rendleman <rendl008@umn.edu>
2023-04-02 11:56:18 -0500

    initial state

--- src/animations/FrogJump.js
+++ src/animations/FrogJump.js
@@ -1,40 +1,46 @@
 import ReactAnime from "react-animejs";
 const {Anime, stagger } = ReactAnime;
 
-export default function FrogJumps(props) {
+export default function FrogJumps() {
     return(
         <Anime
-            control = {props.control}
-            setMeta = {props.setMeta}
             initial={[
                 {
                     targets: ".jumpingFrog",
-                    translateX: 50,
-                    translateY: 50,
-                    scale: 1.25,
-                    delay: stagger(100)
-                },
-                {
-                    targets: ".jumpingFrog",
-                    translateX: 0,
-                    translateY: 0,
-                    scale: 1,
-                    delay: stagger(100)
-                },
-                {
-                    targets: ".jumpingFrog",
-                    translateX: -50,
-                    translateY: 50,
-                    scale: 1.25,
-                    delay: stagger(100)
-                },
-                {
-                    targets: ".jumpingFrog",
-                    translateX: 0,
-                    translateY: 0,
-                    scale: 1,
-                    delay: stagger(100)
+                    keyframes: [
+                        {
+                            // targets: ".jumpingFrog",
+                            translateX: 50,
+                            translateY: 50,
+                            scale: 1.25,
+                            delay: stagger(100)
+                        },
+                        {
+                            // targets: ".jumpingFrog",
+                            translateX: 0,
+                            translateY: 0,
+                            scale: 1,
+                            delay: stagger(100)
+                        },
+                        {
+                            // targets: ".jumpingFrog",
+                            translateX: -50,
+                            translateY: 50,
+                            scale: 1.25,
+                            delay: stagger(100)
+                        },
+                        {
+                            // targets: ".jumpingFrog",
+                            translateX: 0,
+                            translateY: 0,
+                            scale: 1,
+                            delay: stagger(100)
+                        }
+                    ],
+                    loop: true
+
                 }
+                
             ]}
         >
             <img className='jumpingFrog' width='200rem' src='frog.png' alt='frog'></img>
